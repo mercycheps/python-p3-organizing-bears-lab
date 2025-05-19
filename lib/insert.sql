aff204268be4df372a1a6546387098161f8bace2
Mercy <maritimmercy8@gmail.com>
2025-05-19 22:04:34 +0300

    create, insert

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament) VALUES
+("Mr.Chocolate",  6, "F", "Dark", "Calm")
+("Rowdy", 5, "brown", "Angry", "Gentle")
+("Tabitha", 3, "F", "Blonde", "Cheerful"),
+("Sergeant Brown", 7, "F", "Brown", "Disciplined"),
+("Melissa", 5, "M", "Gray", "Kind"),
+("Grinch", 8, "F", "Green", "Grumpy"),
+("Wendy", 2, "M", "White", "Gentle"),
+(NULL, 9, "F", "Dark", "Mysterious");
