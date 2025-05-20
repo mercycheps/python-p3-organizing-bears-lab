ea968ceb5a0af4c2679318d12adfc7be6300fcaa
Mercy <maritimmercy8@gmail.com>
2025-05-20 10:07:26 +0300

    added columns

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
-INSERT INTO bears (name, age, sex, color, temperament) VALUES
-("Mr.Chocolate",  6, "F", "Dark", "Calm")
-("Rowdy", 5, "brown", "Angry", "Gentle")
-("Tabitha", 3, "F", "Blonde", "Cheerful"),
-("Sergeant Brown", 7, "F", "Brown", "Disciplined"),
-("Melissa", 5, "M", "Gray", "Kind"),
-("Grinch", 8, "F", "Green", "Grumpy"),
-("Wendy", 2, "M", "White", "Gentle"),
-(NULL, 9, "F", "Dark", "Mysterious");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Mr.Chocolate",  6, "F", "Dark", "Calm", 1),
+("Rowdy", 5, "brown", "Angry", "Gentle", 1),
+("Tabitha", 3, "F", "Blonde", "Cheerful", 1),
+("Sergeant Brown", 7, "F", "Brown", "Disciplined", 1),
+("Melissa", 5, "M", "Gray", "Kind", 1),
+("Grinch", 8, "F", "Green", "Grumpy", 1),
+("Wendy", 2, "M", "White", "Gentle", 1),
+(NULL, 9, "F", "Dark", "Mysterious", 0);
